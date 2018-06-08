9ecfb52b4ac46d3ee6814a675bae89189aed6344
nazreenrazak <nazreenshahshaiha@gmail.com>
2018-06-08 16:14:09 +0800

    form student, add n edit

--- student/urls.py
+++ student/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^list_json/$', views.student_list_json.as_view(), name="student_list_json"),
     url(r'^home/$',views.home_json,name='student_home_json'),
     url(r'^freelancer/$',views.home_freelancer,name='student_home_freelancer'),
+    url(r'^new/$', views.student_new, name='student_new'),
+    url(r'^(?P<pk>\d+)/$', views.student_detail, name='student_detail'),
 ]
--- student/views.py
+++ student/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 from django.contrib import messages
 # from django.utils import timezone
 from .models import Student
+from .forms import StudentForm
 # from .forms import MessageForm, SearchForm, StudentForm
 # from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
@@ -22,6 +23,30 @@ def home_freelancer(request):
 
 def home_json(request):
     return render(request, 'student/home_json.html')
+
+def student_detail(request,pk):
+    print(Student.objects.all)
+    student = get_object_or_404(Student, pk=pk)
+    print(student)
+    print(student.name)
+    print(student.course)
+    return render(request, 'Student/student_details.html', {'student': student})
+
+def student_new(request):
+
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            student = form.save(commit=False)
+            student.createdby = request.user
+            student.save()
+            messages.success(request, "Student record with ID: " + str(student.pk) + " has been created ! ")
+            # return redirect(reverse_lazy('student_detail',kwargs={'pk': student.pk }))
+    else:
+        form = StudentForm()
+    print(request.user)
+    return render(request, 'student/student_new.html', {'form': form})
+
 
 # Student JSON list filtering
 class student_list_json(BaseDatatableView):
