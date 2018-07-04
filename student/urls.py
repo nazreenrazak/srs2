52465d810c29df98a74e4976c5ef9340ece2e9eb
nazreenrazak <nazreenshahshaiha@gmail.com>
2018-07-04 12:07:41 +0800

    tuka template

--- student/urls.py
+++ student/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^$',views.home,name='student_home'),
-    url(r'^list_json/$', views.student_list_json.as_view()), name="student_list_json"),
-    url(r'^home/$',login_required(views.home_json,name='student_home_json'),
+    url(r'^list_json/$', login_required(views.student_list_json.as_view()), name="student_list_json"),
+    url(r'^home/$',views.home_json,name='student_home_json'),
     url(r'^freelancer/$',views.home_freelancer,name='student_home_freelancer'),
     url(r'^new/$', views.student_new, name='student_new'),
     url(r'^(?P<pk>\d+)/$', views.student_detail, name='student_detail'),
